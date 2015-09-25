9de13fba72ae52c29bc4a649a264e5480ad54be5
CorrieASmith <corriesmith87@gmail.com>
2015-09-25 12:20:26 -0700

    add a new question to the index page

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+
+    save1() {
+      var params = {
+        question: this.get('question'),
+        author: this.get('author'),
+        details: this.get('details'),
+      };
+      this.set('addNewQuestion', false),
+      this.sendAction('save2', params);
+    }
+  }
 });
