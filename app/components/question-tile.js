8292d36e27becf98b22f37448968d113884e9040
CorrieASmith <corriesmith87@gmail.com>
2015-09-25 12:08:00 -0700

    add delete question functionality

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  delete(question) {
-    if (confirm('Surely you cannot be serious about deleting this...can you?')) {
-      this.sendAction('destroyQuestion', question);
+  actions: {
+    delete(question) {
+      if (confirm('Surely you cannot be serious about deleting this...can you?')) {
+        this.sendAction('destroyQuestion', question);
+      }
     }
   }
 });
