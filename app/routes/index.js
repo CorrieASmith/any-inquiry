bbf1c360832eab48ef8ec1bb58e682208c878f26
CorrieASmith <corriesmith87@gmail.com>
2015-09-25 10:39:28 -0700

    add questions object with JavaScript

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
+var questions = [{
+  id: 1,
+  question: "Why can't I own a Canadian?",
+  author: "Ned Flanders",
+  details: "I want to get one soonely doodely."
+}, {
+
+  id: 2,
+  question: "If you die, what happens to your Myspace?",
+  author: "Chris Griffin",
+  details: "Does it delete itself or do your friends have to?"
+}, {
+
+  id: 3,
+  question: "How Can Mirrors Be Real If Our Eyes Aren't Real.",
+  author: "Jaden Smith",
+  details: "When I Die. Then You Will Realize"
+}];
+
 export default Ember.Route.extend({
+  model() {
+    return questions;
+  },
 });
