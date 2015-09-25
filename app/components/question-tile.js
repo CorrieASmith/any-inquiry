7c2b9f235b648b2230c653cde6536bd61edc881e
CorrieASmith <corriesmith87@gmail.com>
2015-09-25 11:47:53 -0700

    add delete method; fix bugs

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  delete(question) {
+    if (confirm('Surely you cannot be serious about deleting this...can you?')) {
+      this.sendAction('destroyQuestion', question);
+    }
+  }
 });
