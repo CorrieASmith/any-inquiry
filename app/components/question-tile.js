70ca60681463dda115ea16168d1aff3b08a8ec52
CorrieASmith <corriesmith87@gmail.com>
2015-09-25 14:12:32 -0700

    update component functional for all but question name; fix bug

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateQuestionForm: false,
   actions: {
+    update(question,params) {
+      this.sendAction('update', question, params);
+    },
     delete(question) {
       if (confirm('Surely you cannot be serious about deleting this...can you?')) {
         this.sendAction('destroyQuestion', question);
--- app/routes/index.js
+++ app/routes/index.js
@@ -32,6 +32,11 @@ export default Ember.Route.extend({
     },
 
     update(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          question.set(key, params[key]);
+        }
+      });
       question.save();
       this.transitionTo('index');
     },
