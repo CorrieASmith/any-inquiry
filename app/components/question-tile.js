aa08d3b0a7b8831689877282b67da057bd00cde4
Corrie Smith <student@epicodus.com>
2015-09-30 11:59:50 -0700

    fix update question bug

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   updateQuestionForm: false,
   actions: {
-    update(question,params) {
+    update(question, params) {
       this.sendAction('update', question, params);
     },
     delete(question) {
